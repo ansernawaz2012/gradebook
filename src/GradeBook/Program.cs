5b7c4c9dd80fa9f6d69d8e5eaf022bbc528965be
ansernawaz2012 <ansernawaz2012@gmail.com>
2019-05-02 10:10:51 +0100

    Add project files.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IBook book = new DiskBook("Anser's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+
+            EnterGrades(book);
+
+            /* book.AddGrade(89.1);
+            book.AddGrade(75.2);
+            book.AddGrade(79.2);
+            book.AddGrade(82.3);
+            book.AddGrade(88.4); */
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+
+            Console.ReadKey();
+
+
+
+
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)   
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
+        }
+    }
+}
